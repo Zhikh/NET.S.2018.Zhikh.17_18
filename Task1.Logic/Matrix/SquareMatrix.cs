d1a4d0a58ed4d59d0a53cdf7980d8f975ec4a1e9
Domino <zhikhanastasya@gmail.com>
2018-07-29 23:55:46 +0300

    Add file

---
+++ Task1.Logic/Matrix/SquareMatrix.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Task1.Logic
+{
+    public class SquareMatrix<T> : BaseSquareMatrix<T>
+    {
+        /// <summary>
+        /// Initialize object of an n-by-n matrix is known as a square matrix of order n.
+        /// </summary>
+        /// <param name="order"> Number of elements in row/column /</param>
+        public SquareMatrix(int order, IComparer<T> comparer = null) : base(order, comparer)
+        {
+        }
+    }
+}
