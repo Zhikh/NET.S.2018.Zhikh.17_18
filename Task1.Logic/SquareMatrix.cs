ea2cd48b9f3c2646a86d7dff3eca1b2a244a1ab5
Domino <zhikhanastasya@gmail.com>
2018-07-29 15:29:24 +0300

    Add files

---
+++ Task1.Logic/SquareMatrix.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1.Logic
+{
+    public class SquareMatrix<T>
+    {
+        private T[,] _container;
+
+        /// <summary>
+        /// Initialize object of an n-by-n matrix is known as a square matrix of order n.
+        /// </summary>
+        /// <param name="order"> Number of elements in row/column /</param>
+        public SquareMatrix(int order)
+        {
+            if (order <= 0)
+            {
+                throw new ArgumentException($"The {nameof(order)} can't be less than 0 or be 0!");
+            }
+
+            _container = new T[order, order];
+            Order = order;
+        }
+
+        public int Order { get; }
+
+        public T this[int indexRow, int indexColumn]
+        {
+            get
+            {
+                return _container[indexRow, indexColumn];
+            }
+
+            set
+            {
+                _container[indexRow, indexColumn] = value;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Order; i++)
+            {
+                for (int j = 0; j < Order; j++)
+                {
+                    yield return this[i, j];
+                }
+            }
+        }
+        #region Private methods
+
+        #endregion
+    }
+}
---
+++ Task1.Tests/SquareMatrixTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Task1.Logic;
+
+namespace Task1.Tests
+{
+    [TestFixture]
+    public class SquareMatrixTests
+    {
+        #region Exceptions
+        [Test]
+        public void SquareMatrix_ZeroOrder_ArgumentException()
+            => Assert.Catch<ArgumentException>(() => new SquareMatrix<int>(0));
+
+        [Test]
+        public void SquareMatrix_NegativeOrder_ArgumentException()
+            => Assert.Catch<ArgumentException>(() => new SquareMatrix<int>(-1));
+        #endregion
+
+        #region Add value to matrix
+        [TestCase(2, new int[] { 1, 2, 3, 4})]
+        public void Indexator_SequenceOfElements_CorrectResult(int order, int[] values)
+        {
+            var matrix = new SquareMatrix<int>(order);
+            AddValues(order, values, matrix);
+
+            int i = 0;
+            foreach (var element in matrix)
+            {
+                Assert.AreEqual(values[i++], element);
+            }
+        }
+        #endregion
+
+        #region Additional methods
+        private static void AddValues<T>(int order, T[] values, SquareMatrix<T> matrix)
+        {
+            int k = 0;
+            for (int i = 0; i < order; i++)
+            {
+                for (int j = 0; j < order; j++)
+                {
+                    matrix[i, j] = values[k++];
+                }
+            }
+        }
+        #endregion
+    }
+}
