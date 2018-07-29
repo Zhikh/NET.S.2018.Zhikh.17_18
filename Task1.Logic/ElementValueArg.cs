fdfa22bda90f9ee919921f2e71731aeaccf385bb
Domino <zhikhanastasya@gmail.com>
2018-07-29 22:23:12 +0300

    Add file

---
+++ Task1.Logic/ElementValueArg.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task1.Logic
+{
+    public class ElementValueArg<T> : EventArgs
+    {
+        public ElementValueArg(T oldElement, T newElement)
+        {
+
+        }
+        
+        public T OldValue { get; }
+        public T NewValue { get; }
+    }
+}
