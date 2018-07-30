98d0f170854366337eb0e14b9072d491f4f1bf75
Domino <zhikhanastasya@gmail.com>
2018-07-30 22:41:22 +0300

    Add file

---
+++ Task1.Logic/Exceptions/InvalidMatrixOperationException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Task1.Logic.Exceptions
+{
+    class InvalidMatrixOperationException : ApplicationException
+    {
+        public InvalidMatrixOperationException() { }
+
+        public InvalidMatrixOperationException(string message) : base(message) { }
+
+        public InvalidMatrixOperationException(string message, Exception inner) : base(message, inner) { }
+
+        protected InvalidMatrixOperationException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
